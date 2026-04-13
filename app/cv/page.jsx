487fdfc3e32122c8af93d46b6859e49e466083bd
Ahmed Saad Tanim <ahmedsaadtanim@gmail.com>
2026-04-13 14:16:17 +0600

    Add academic cv

--- app/cv/page.jsx
+++ app/cv/page.jsx
@@ -6,10 +6,24 @@ const CV = () => {
   return (
     <Section heading="Curriculum Vitae">
       <p className="mt-4 mb-2 text-lg">
+        You can download a PDF copy of my {" "}
+        <ClickableLink
+          linkText="Academic CV"
+          link="https://drive.google.com/file/d/1QXH14SuVeZdE6yb_fiC72cDVqsAZD9HK/view"
+        />{" "}
+        here
+      </p>
+      <iframe
+        src="https://drive.google.com/file/d/1QXH14SuVeZdE6yb_fiC72cDVqsAZD9HK/preview"
+        className="w-full h-[calc(100dvh-20rem)] border-none"
+        title="PDF Viewer"
+      ></iframe>
+
+      <p className="mt-4 mb-2 text-lg">
         You can download a PDF copy of my{" "}
         <ClickableLink
-          linkText="CV"
-          link="https://drive.google.com/file/d/1S1dtDx_U1FLnoQjIS2KAhNmStbroy187/view?usp=sharing"
+          linkText="Work CV"
+          link="https://drive.google.com/file/d/1S1dtDx_U1FLnoQjIS2KAhNmStbroy187/view"
         />{" "}
         here
       </p>
